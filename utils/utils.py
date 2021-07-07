ead24210133231f523ad0b2ab2f426442a5839b7
MaximovaIrina <irina.maximovva@gmail.com>
2021-07-07 14:40:52 +0300

    add plot_pixel_hists_fe_classes

--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,9 @@ import torch
 import cv2
 import os
 
+from scipy.interpolate import make_interp_spline
 from skimage.feature import greycomatrix, greycoprops
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 from models.features import Features
@@ -111,7 +113,7 @@ def cimbine_features(channel='R', dst_root='comb'):
     torch.save(data, file)
 
 
-def getLocFeachAsImage(img_pth):
+def get_loc_feach_as_image(img_pth):
     PI = np.pi
     MODE, CHANNEL = 'local', 'ndvi'
     bins, dist = [-2, -1, 0, 1, 2], [1, 4, 8]
@@ -129,7 +131,7 @@ def getLocFeachAsImage(img_pth):
     torch.save(features, '..\\data\\Healthy_sample.pth')
 
 
-def healthyImgFilter(root):
+def healthy_img_filter(root):
     files = os.listdir(root)
     i = 0
     for file in tqdm(files):
@@ -151,3 +153,53 @@ def healthyImgFilter(root):
         # Balance dataset
         if i > 1000:
             break
+
+
+def plot_pixel_hists_fe_classes(root, delta=5):
+    dirs = os.listdir(root)
+    data = {k: group_flatten(os.path.join(root, k), delta) for k in tqdm(dirs)}
+    bins = np.arange(0, 1, 0.1)
+    hists = {k: np.histogram(data[k], bins)[0] for k in tqdm(dirs)}
+    for i, k in enumerate(dirs):
+        x = np.linspace(0, 1, len(hists[k]))
+        hist = np.asarray(hists[k])
+        hist = hist / hist.sum()
+        spl = make_interp_spline(x, hist, k=2)
+        xnew = np.linspace(0, 1, 100)
+        power_smooth = spl(xnew)
+        plt.plot(xnew, power_smooth, label=k)
+
+    xticklabels = ['-3σ', '-2σ', '-σ', '0', 'σ', '2σ', '3σ']
+    bins = np.arange(-3, 4)
+    loc = np.full((len(bins),), 0.5) + np.asarray(bins) * 1./6.
+    plt.xticks(loc, xticklabels)
+
+    _, t = plt.ylim()
+    plt.ylim(0, t)
+    plt.xlabel('Интенсивность')
+    plt.ylabel('Вероятность')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('plot\\accum_hist.png')
+
+
+def group_flatten(root, delta, mean=99.267, std=43):
+    files = os.listdir(root)
+    data = []
+    for file in files:
+        img = cv2.imread(os.path.join(root, file))
+        img = img[:, :, 2]
+        img = img[img > delta]
+
+        # ''' ndvi '''
+        # red = img[:, :, 2]
+        # green = img[:, :, 1]
+        # ndvi = (green - red) / (green + red + 1e-9)
+        # ndvi[(red == 0) & (green == 0)] = 0
+        # ndvi = ndvi[(ndvi != 0) & (-0.95 < ndvi) & (ndvi < 0.95)]
+        # img = ndvi
+
+        img = (img - mean) / std
+        img = (img + 3) / 6
+        data += list(img)
+    return data
